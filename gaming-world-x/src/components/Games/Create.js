09bacdeccc687c9423bcb841cf12660655036744
BozhidarChervenkov <bojidar.chervenkov@abv.bg>
2022-08-11 15:01:38 +0300

    Reworked the create form so that it is controlled now and with validation

--- gaming-world-x/src/components/Games/Create.js
+++ gaming-world-x/src/components/Games/Create.js
@@ -1,17 +1,62 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { GameContext } from '../../contexts/GameContext';
 import * as gameService from '../../services/gameService';
+import isURL from 'validator/lib/isURL';
 
 const Create = () => {
+    // Validation:
+    const [errors, setErrors] = useState({});
+    const [fieldValues, setFieldValues] = useState({
+        title: '',
+        description: '',
+        price: '',
+        imageUrl: '',
+        videoUrl: '',
+        category: 'Action'
+    });
+
+    const changeValidationHandler = (e) => {
+        setFieldValues(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }));
+    };
+
+    const minLength = (e, bound) => {
+        setErrors(state => ({
+            ...state,
+            [e.target.name]: fieldValues[e.target.name].length < bound,
+        }));
+    }
+
+    const isPositive = (e) => {
+        let number = Number(e.target.value);
+        setErrors(state => ({
+            ...state,
+            [e.target.name]: number < 0,
+        }));
+    }
+
+    const validateWebsiteUrl = (e) => {
+        let url = e.target.value;
+        setErrors(state => ({
+            ...state,
+            [e.target.name]: !isURL(url),
+        }));
+    };
+
+    const isFormValid = !Object.values(errors).some(x => x)
+
+    // Main logic:
     const navigate = useNavigate();
     const { gameAdd } = useContext(GameContext);
 
     const onSubmit = (e) => {
         e.preventDefault();
 
-        const gameData = Object.fromEntries(new FormData(e.target));
+        const gameData = fieldValues;
 
         gameService.create(gameData)
             .then(result => {
@@ -36,7 +81,11 @@ const Create = () => {
                         type="text"
                         name="title"
                         className="form-control"
+                        value={fieldValues.title} onChange={changeValidationHandler} onBlur={(e) => minLength(e, 3)}
                     />
+                    {errors.title &&
+                        <p className="form-error">Title length should be at least 3 characters!</p>
+                    }
                 </div>
 
                 <div className="mb-3">
@@ -47,7 +96,11 @@ const Create = () => {
                         type="text"
                         name="description"
                         className="form-control"
+                        value={fieldValues.description} onChange={changeValidationHandler} onBlur={(e) => minLength(e, 15)}
                     />
+                    {errors.description &&
+                        <p className="form-error">Title description should be at least 15 characters!</p>
+                    }
                 </div>
 
                 <div className="mb-3">
@@ -58,7 +111,11 @@ const Create = () => {
                         type="text"
                         name="price"
                         className="form-control"
+                        value={fieldValues.price} onChange={changeValidationHandler} onBlur={isPositive}
                     />
+                    {errors.price &&
+                        <p className="form-error">Price should be a positive number!</p>
+                    }
                 </div>
 
                 <div className="mb-3">
@@ -69,7 +126,11 @@ const Create = () => {
                         type="text"
                         name="imageUrl"
                         className="form-control"
+                        value={fieldValues.imageUrl} onChange={changeValidationHandler} onBlur={validateWebsiteUrl}
                     />
+                    {errors.imageUrl &&
+                        <p className="form-error">Invalid image URL!</p>
+                    }
                 </div>
 
                 <div className="mb-3">
@@ -80,12 +141,16 @@ const Create = () => {
                         type="text"
                         name="videoUrl"
                         className="form-control"
+                        value={fieldValues.videoUrl} onChange={changeValidationHandler} onBlur={validateWebsiteUrl}
                     />
+                    {errors.videoUrl &&
+                        <p className="form-error">Invalid video URL!</p>
+                    }
                 </div>
 
                 <div className="mb-3">
                     <label htmlFor='category'>Select Category:</label>
-                    <select name='category' id='category'>
+                    <select name='category' id='category' value={fieldValues.category} onChange={changeValidationHandler}>
                         <option value='Action'>Action</option>
                         <option value='Adventure'>Adventure</option>
                         <option value='Racing'>Racing</option>
@@ -97,7 +162,7 @@ const Create = () => {
 
                 <hr />
 
-                <button type="submit" className="btn btn-primary mb-5">
+                <button type="submit" className="btn btn-primary mb-5" disabled={!isFormValid}>
                     Add Game
                 </button>
             </div>
